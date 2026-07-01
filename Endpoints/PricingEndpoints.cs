2d9c879ba147c2f3077c3e6ab09c7e3f382b846b
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2026-07-01 09:57:38 +0100

    Add batch processing endpoint

--- Endpoints/PricingEndpoints.cs
+++ Endpoints/PricingEndpoints.cs
@@ -6,6 +6,8 @@ public static class PricingEndpoints
 {
     public static void MapPricingEndpoints(this WebApplication app)
     {
+
+        // POST /price
         app.MapPost("/price", (PricingRequest req, IEnumerable<IInstrumentPricer> pricers) =>
         {
             foreach (var p in pricers)
@@ -16,5 +18,37 @@ public static class PricingEndpoints
 
             return Results.BadRequest($"Unknown instrument type: {req.InstrumentType}");
         });
+
+
+
+        // POST /price/batch
+        app.MapPost("/price/batch", async (PricingRequest[] requests, IEnumerable<IInstrumentPricer> pricers) =>
+        {
+            List<Task<PricingResult>> allTasks = [];
+
+            try
+            {
+                foreach (var req in requests)
+                    allTasks.Add(PriceOneAsync(req, pricers));
+
+                await Task.WhenAll(allTasks);
+                return Results.Ok(allTasks.Select(task => task.Result));
+            }
+            catch (BadHttpRequestException e)
+            {
+                return Results.BadRequest($"{e.Message}");
+            }
+        });
+    }
+
+    async private static Task<PricingResult> PriceOneAsync(PricingRequest req, IEnumerable<IInstrumentPricer> pricers)
+    {
+        await Task.Delay(30000);    // 30 seconds
+        foreach (var p in pricers)
+        {
+            if (p.InstrumentType == req.InstrumentType)
+                return p.Price(req);
+        }
+        throw new BadHttpRequestException($"Unknown instrument type: {req.InstrumentType}");
     }
 }
